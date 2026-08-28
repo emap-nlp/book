be2561b0690b99a2343a4fd848f79913202d9efc
Alexandre Rademaker <arademaker@gmail.com>
2026-08-27 23:25:22 -0300

    Atualiza a partir de CSwL@a0d7550

---
+++ -verso-search/searchIndex_79.2357306eefe5640e.js
@@ -0,0 +1,1 @@
+window.docContents[79].resolve({"/Applications/#Applications":{"contents":"Três exemplos de PLN que aplicam o Lean visto no capítulo anterior:\nharmonia vocálica do finlandês, plural do sueco e uma representação de\nfonemas por traços.\n\n\n\n\n\n\n\n","context":"Semântica computacional com Lean","header":"3. Aplicações","id":"/Applications/#Applications"}});
